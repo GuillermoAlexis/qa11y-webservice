c04083d63e92e4beae0f66b9660b54e00856b2dd
Guillermo <guillermo-alexis@hotmail.com>
2019-05-14 02:20:41 -0400

    cambios db

--- config.js
+++ config.js
@@ -21,9 +21,8 @@ if (fs.existsSync(jsonPath)) {
 	module.exports = require(jsonPath);
 } else {
 	module.exports = {
-		database: env('DATABASE', 'mongodb://localhost/pa11y-webservice'),
-		host: env('HOST', '0.0.0.0'),
-		port: Number(env('PORT', '3000')),
+		database: env('DATABASE', 'mongodb://qa11ywebservicebd:b51010786d405f6b7c58b1044392283c@dokku-mongo-qa11ywebservicebd:27017/qa11ywebservicebd'),
+		host: env('HOST', '172.17.0.3'),
 		cron: env('CRON', false)
 	};
 }
